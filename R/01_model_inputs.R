e14502e5115391de40d3ac3e5292903c17dc7809
seleneee2002 <linxiyao2002@gmail.com>
2025-03-26 20:27:36 -0400

    Remove T21-related inputs
    Removed T21-specific input parameters: `v_policy.ages`, `policycohort`,`v_mla.effects`,`v_policy.scen`

--- R/01_model_inputs.R
+++ R/01_model_inputs.R
@@ -1,10 +1,10 @@
 ### CISNET Population Model
-### Model of T21 policy for all US states
+### Model for tax
 ### Code by Rafael Meza modified by Lyss Crippen, Jamie Tam
 ### Use state_process_inputs.R to complete data processing and generate inputs for each state 
 
 rm(list = ls()) 
-setwd("/Users/ac3456/Dropbox/state_tcp_tool/LC_code/TCP_model/")
+setwd("/Users/wangmengyao/Documents/GitHub/tcp-model-code/")
 
 # Package names
 packages <- c("ggplot2", "readxl", "readr", "dplyr", "tidyr", "reshape2","grid", 
@@ -19,7 +19,7 @@ if (any(installed_packages == FALSE)) {
 lapply(packages, library, character.only = TRUE)
 
 #---------------------- Load data sets --------------------------------------------------------
-load("data/T21policycoverage2005.2025.Rda") # T21 policy coverage data
+
 
 load('data/harmonized_tuscps_prevs.Rda') # TUSCPS survey data for model verification
 
@@ -33,21 +33,21 @@ make_tcp_out <- 0
 
 #other model inputs
 
-v_policy.ages <- c(18:20) # ages affected by the policy
+
 date_variable <- format(Sys.Date(), "%m.%d.%y")
 startbc <- 1908   # starting birth cohort 
 endbc <- 2100     # ending birth cohort
 endyear <- 2200   # final calendar year, extra 100 years needed for AC-AP conversion
-policycohort <- 1985 # first birth cohort affected by T21 in the year 2005 is 1985
+
 policyyear=2005   # policy year, NA for T21 analysis
 cohyears <- endbc-startbc+1     # number of cohort years
 totalyears<-length(startbc:endyear)
 v_calyears <- (startbc-startbc+1):(endbc-startbc+1)  # index of calendar years 1908-2100
 v_stdbirths<- rep(1000000, times = 193)    #fixed birthrate/population size for prevalence calculations
-v_mla.effects= c(main=0.3391,upper=0.5266,lower=0.1517,baseline=0)  # main T21 policy effect estimate with upper, lower bounds
 
-#tiers of policy coverage scenarios: local only, state&local, combined federal&state&local, and baseline (no coverage)
-v_policy.scen= c("local","statelocal","fedstatelocal","baseline")
+
+
+
 
 # vector of states to run simulations for (50 states + DC)
 v_statefips <- c('01','02','04','05','06','08','09','10','11','12','13','15','16',
