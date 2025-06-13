91acc195420760d27fab57639d7916ad4a78d01b
Selene Wang <linxiyao2002@gmail.com>
2025-06-13 14:08:36 +0800

    Refactor initialization and load general state-level inputs
    1)Added loop to load state-specific inputs: life expectancy, population, mortality, and smoking data
    2)Updated model input initialization for clarity and general use

--- R/01_model_inputs.R
+++ R/01_model_inputs.R
@@ -1,7 +1,4 @@
 ### CISNET Population Model
-### Model for tax
-### Code by Rafael Meza modified by Lyss Crippen, Jamie Tam
-### Use state_process_inputs.R to complete data processing and generate inputs for each state 
 
 rm(list = ls()) 
 setwd("/Users/wangmengyao/Documents/GitHub/tcp-model-code/")
@@ -20,8 +17,13 @@ lapply(packages, library, character.only = TRUE)
 
 #---------------------- Load data sets --------------------------------------------------------
 
-
-load('data/harmonized_tuscps_prevs.Rda') # TUSCPS survey data for model verification
+for (f in v_statefips) {
+  load(paste0("data/state_inputs/le_",f,".RData"))
+  load(paste0("data/state_inputs/pop_",f,".RData"))
+  load(paste0("data/state_inputs/mort_rates/mort_",f,".RData"))
+  load(paste0("data/state_inputs/mort_rates/p.mort_", f, ".RData"))
+  load(paste0("data/state_inputs/smk_",f,".RData"))
+}
 
 #-------------------- Set model inputs -----------------------------------------------------------
 ##### Scenario indicators 
@@ -30,35 +32,20 @@ load('data/harmonized_tuscps_prevs.Rda') # TUSCPS survey data for model verifica
 policy_decay <- 0
 make_tcp_out <- 0
 
-
 #other model inputs
-
 
 date_variable <- format(Sys.Date(), "%m.%d.%y")
 startbc <- 1908   # starting birth cohort 
 endbc <- 2100     # ending birth cohort
 endyear <- 2200   # final calendar year, extra 100 years needed for AC-AP conversion
 
-policyyear=2005   # policy year, NA for T21 analysis
 cohyears <- endbc-startbc+1     # number of cohort years
 totalyears<-length(startbc:endyear)
 v_calyears <- (startbc-startbc+1):(endbc-startbc+1)  # index of calendar years 1908-2100
 v_stdbirths<- rep(1000000, times = 193)    #fixed birthrate/population size for prevalence calculations
-
-
-
-
 
 # vector of states to run simulations for (50 states + DC)
 v_statefips <- c('01','02','04','05','06','08','09','10','11','12','13','15','16',
                  '17','18','19','20','21','22','23','24','25','26','27','28','29',
                  '30','31','32','33','34','35','36','37','38','39','40','41','42',
                  '44','45','46','47','48','49','50','51','53','54','55','56')
-
-# vector of states to generate main manuscript figures for
-#paperfips=c('06','21','25','55') # CA, KY, MA, WI 
-
-#-- run the model looping through all states -------------------------------------
-source('R/02_model_functions.R')
-source('R/03_main_analysis.R')
-source('R/04_visualization.R')
